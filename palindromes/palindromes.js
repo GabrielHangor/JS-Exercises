e443872036aed60a2197e736262cec92e59644ec
HangorG <gaba93@yandex.ru>
2021-01-07 00:09:22 +0300

    Palindrome done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
+const palindromes = function (string) {
+  const ourArray = [...string]
+    .filter((element) =>element !== "!" && element !== "." && element !== "," && element !== " ")
+    .map((word) => word.toLowerCase())
+    .join('');
 
-}
+  const reversedArray = [...string]
+    .reverse()
+    .filter((element) =>element !== "!" && element !== "." && element !== "," && element !== " ")
+    .map((word) => word.toLowerCase())
+    .join('');
 
-module.exports = palindromes
+  console.log(ourArray, reversedArray);
+  return ourArray === reversedArray ? true : false;
+};
+
+palindromes("A car, a man, a maraca.");
+
+module.exports = palindromes;
