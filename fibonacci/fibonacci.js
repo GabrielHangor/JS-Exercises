15b6e65fc3850a15c15eed28db71de020c540582
HangorG <gaba93@yandex.ru>
2021-01-07 20:00:07 +0300

    caesar and fibo

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const ourArray = [1, 1];
+  if (n == 1 || n == 2) {
+    return 1;
+  } else if (n < 0) {
+    return "OOPS";
+  }
 
-}
+  for (let i = 2; i <= n; i++) {
+    ourArray.push(ourArray[i - 1] + ourArray[i - 2]);
+  }
 
-module.exports = fibonacci
+  return ourArray[n - 1];
+};
+
+module.exports = fibonacci;
