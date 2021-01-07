6abc7d6836d6b5bd3d9c8602d25bd7846dd785ab
HangorG <gaba93@yandex.ru>
2021-01-08 01:07:30 +0300

    findTheOldest completed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
 
-}
+let findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  });
 
-module.exports = findTheOldest
+  const oldest = people.sort((a, b) => {
+    return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+  });
+  return oldest[oldest.length - 1];
+};
+
+
+
+module.exports = findTheOldest;
